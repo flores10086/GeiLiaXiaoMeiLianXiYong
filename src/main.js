13447bfbfbcf3308a5798f288ae6a14b97fd3b2f
RTT <rtt622727@qq.com>
2026-03-26 00:25:53 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,5 +3,23 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import router from './router'
 
-createApp(App).use(ElementPlus).mount('#app')
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.use(ElementPlus, {
+    locale: zhCn,
+})
+
+
+
+app.use(router).use(ElementPlus).mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import {
+    createRouter, createWebHashHistory
+} from 'vue-router'
+
+const routs = [{
+
+}]
+
+const router = creatrRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router 
