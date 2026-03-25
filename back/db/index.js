1807eabe675d728efe71c176ba132b9dc2381aa0
RTT <rtt622727@qq.com>
2026-03-25 17:39:13 +0800

    rtt配置后端

--- back/db/index.js
+++ back/db/index.js
@@ -2,9 +2,9 @@ const mysql = require('mysql')
 
 const db = mysql.createPool({
     host: '127.0.0.1',
-    user: 'xds',
-    password: 'hdjz30536',
-    database: 'back_system'
+    user: 'root',
+    password: '123456',
+    database: 'mydb'
 });
 
 module.exports = db
