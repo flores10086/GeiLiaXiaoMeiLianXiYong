dd0a700eb4ed911a3f95ad8c450005070375b493
RTT <rtt622727@qq.com>
2026-04-03 00:40:19 +0800

    4.2

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -9,16 +9,33 @@
       </el-header>
       <el-main>
         <div class="login-wrapped">
-
-
+            <el-card class="box-card">
+               <el-tabs v-model="activeName" class="demo-tabs" :stretch="'true'">
+               <el-tab-pane label="登录" name="first">登录</el-tab-pane>
+               <el-tab-pane label="注册" name="second">注册</el-tab-pane>
+               
+              
+  </el-tabs>
+            </el-card>
         </div>
       </el-main>
-      <el-footer>Footer</el-footer>
+      <el-footer class="footer-wrapped">
+        <div class="footer-content">
+          <div class="title">
+            <span>阿里云博客专家</span> |
+            <span>阿里云博客专家</span> |
+            <span>阿里云博客专家</span>
+          </div>
+        </div>
+
+      </el-footer>
     </el-container>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+const activeName = ref('first')
 </script>
 
 <style lang="scss" scoped>
@@ -37,6 +54,38 @@
     .el-main{
       background-image: url('@/assets/code.jpg');
       height: 600px;
+      --el-main-padding:0;
+
+      .login-wrapped{
+        width: 1200px;
+            height: 600px;
+            margin: 0 auto;
+
+
+            .box-card{
+               width: 350px;
+               height: 375px;
+               float: right;
+               position: relative;
+               top: 14%;
+            }
+      }
      }
-     
+     .footer-wrapped{
+      margin-top: 8px;
+      .footer-content{
+        width: 1200px;
+          margin: 0 auto;
+          display: flex;
+          justify-content: center;
+
+          .title{
+            color: #666;
+          }
+          span{
+            color: #666;
+            font-size: 12px;
+          }
+      }
+     }
 </style>
